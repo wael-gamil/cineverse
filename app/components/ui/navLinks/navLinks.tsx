69ec6db4723b71cefe1c450bb9433edd217d8528
Wael <131612777+ggwael8@users.noreply.github.com>
2025-05-22 17:32:18 +0300

    navlinks component created

---
+++ app/components/ui/navLinks/navLinks.tsx
@@ -0,0 +1,39 @@
+import styles from './navLinks.module.css';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type NavLinksProps = {
+  isMobile?: boolean;
+  closeMenu?: () => void;
+};
+
+const NAV_LINKS = [
+  { name: 'Home', path: '/' },
+  { name: 'Movies', path: '/movies' },
+  { name: 'TV Series', path: '/tv-series' },
+  { name: 'Reviews', path: '/reviews' },
+  { name: 'Watchlist', path: '/watchlist' },
+];
+export default function NavLinks({
+  isMobile = false,
+  closeMenu,
+}: NavLinksProps) {
+  const pathname = usePathname();
+  return (
+    <ul className={isMobile ? styles.navbar_MobileLinks : styles.navbar_Links}>
+      {NAV_LINKS.map(({ name, path }) => (
+        <li key={name}>
+          <Link
+            href={path}
+            className={`${styles.navbar_item} ${
+              pathname === path ? styles.active : ''
+            }`}
+            onClick={isMobile ? closeMenu : undefined}
+          >
+            {name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
