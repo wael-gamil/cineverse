4abb232d1e1a347f3a4fa767392cfc1d9834e740
Wael <131612777+ggwael8@users.noreply.github.com>
2025-06-07 22:12:40 +0300

    add image configuration and update dependencies for improved image handling and state management

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/w500/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
