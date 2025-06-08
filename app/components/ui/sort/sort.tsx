83d091ca0eec09f8a11a4a9e1b3f735cc9975a10
Wael <131612777+ggwael8@users.noreply.github.com>
2025-06-09 02:22:21 +0300

    refactor: separate Sort from Filter and unify panel UI using PanelWrapper

---
+++ app/components/ui/sort/sort.tsx
@@ -0,0 +1,69 @@
+'use client';
+import styles from './sort.module.css';
+import Button from '../button/button';
+import { Icon } from '../icon/icon';
+import PanelWrapper from '../panelWrapper/panelWrapper';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+type SortProps = {
+  initialSortBy: string;
+};
+export default function Sort({ initialSortBy }: SortProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [sortBy, setSortBy] = useState<string>(initialSortBy || '');
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams.toString());
+    // Update sortBy in URL
+    if (sortBy) {
+      params.set('sortBy', sortBy);
+    } else {
+      params.delete('sortBy');
+    }
+
+    router.push(`?${params.toString()}`, { scroll: false });
+  }, [sortBy, router, searchParams]);
+
+  // Sort options
+  const sortOptions = [
+    { label: 'Most Recent', value: 'mostRecent' },
+    { label: 'Top Rated', value: 'topRated' },
+  ];
+  return (
+    <PanelWrapper
+      label='Sort by'
+      icon={isOpen => (
+        <Icon
+          name={isOpen ? 'close' : 'sliders-horizontal'}
+          width={16}
+          strokeColor='primary'
+        />
+      )}
+      badge={sortOptions.find(o => o.value === sortBy)?.label}
+      position='right'
+    >
+      <div className={styles.options}>
+        {sortOptions.map(option => (
+          <Button
+            key={option.value}
+            type='button'
+            variant={sortBy === option.value ? 'solid' : 'ghost'}
+            color={sortBy === option.value ? 'primary' : 'neutral'}
+            onClick={() => setSortBy(option.value)}
+            width='100%'
+            align='left'
+          >
+            {sortBy === option.value ? (
+              <Icon name='dot' width={16} strokeColor='white' />
+            ) : (
+              <Icon name='dot' width={16} hide />
+            )}
+            {option.label}
+          </Button>
+        ))}
+      </div>
+    </PanelWrapper>
+  );
+}
