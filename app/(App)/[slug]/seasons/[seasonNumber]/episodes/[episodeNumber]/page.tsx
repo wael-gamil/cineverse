32018c44246aa796f15d20a067abfafded1c8194
Wael <131612777+ggwael8@users.noreply.github.com>
2025-06-17 09:33:49 +0300

    feat: add Episode page component to display episode details and reviews

---
+++ app/(App)/[slug]/seasons/[seasonNumber]/episodes/[episodeNumber]/page.tsx
@@ -0,0 +1,44 @@
+import {
+  getContentDetails,
+  getSeasonDetails,
+  getEpisodeDetails,
+  getContentReviews,
+} from '@/app/lib/api';
+import ContentHero from '@/app/components/shared/contentDetails/contentHero';
+import { notFound } from 'next/navigation';
+import { normalizeContent } from '@/app/constants/types/movie';
+
+export default async function Episode({
+  params,
+}: {
+  params: { episodeNumber: number; seasonNumber: number; slug: string };
+}) {
+  const contentDetails = await getContentDetails(params.slug);
+  if (contentDetails.type !== 'series') {
+    notFound();
+  }
+  const seasonDetails = await getSeasonDetails(
+    contentDetails.id,
+    params.seasonNumber
+  );
+  if (seasonDetails.numberOfEpisodes < params.episodeNumber) {
+    notFound();
+  }
+  const episodeDetails = await getEpisodeDetails(
+    contentDetails.id,
+    params.seasonNumber,
+    params.episodeNumber
+  );
+  const contentReviews = await getContentReviews(episodeDetails.id);
+
+  console.log(contentReviews);
+  return (
+    <>
+      <ContentHero
+        content={normalizeContent(episodeDetails)}
+        backgroundUrl={contentDetails.backdropPath}
+        genres={contentDetails.genres}
+      />
+    </>
+  );
+}
