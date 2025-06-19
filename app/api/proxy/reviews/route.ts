c33e16b4166c3a33dee3e6188eed817aa1215bce
Wael <131612777+ggwael8@users.noreply.github.com>
2025-06-20 01:24:06 +0300

    feat: add proxy routes for credits, episodes, reviews, and seasons with error handling

---
+++ app/api/proxy/reviews/route.ts
@@ -0,0 +1,31 @@
+import { Review } from '@/app/constants/types/movie';
+export async function GET(req: Request) {
+  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL as string;
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get('id');
+
+  if (!id) return new Response('Missing id', { status: 400 });
+
+  try {
+    const url = `${BASE_URL}contents/${id}/reviews`;
+    const res = await fetch(url);
+    if (!res.ok) {
+      // Log or forward the status
+      return new Response(`Failed to fetch reviews from upstream ${url}`, {
+        status: res.status,
+      });
+    }
+
+    const data: Review[] = await res.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Proxy error:', error);
+    return new Response('Internal Server Error in proxy', { status: 500 });
+  }
+}
