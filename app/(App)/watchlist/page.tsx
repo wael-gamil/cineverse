f569fa83a70759d7881fcbc96ded9c2eb13dea45
Wael <131612777+ggwael8@users.noreply.github.com>
2025-07-26 22:49:55 +0300

    feat: add watchlist page with styled layout and filter functionality

--- app/(App)/watchlist/page.tsx
+++ app/(App)/watchlist/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+import styles from './page.module.css';
+import WatchlistList from '../../../components/shared/watchlist/watchlistList';
+import SectionHeader from '@/components/shared/contentSliderSection/sectionHeader';
+import FilterTabs from '@/components/ui/filter/filterTabs';
+import { useState } from 'react';
+
+const WATCHLIST_STATUSES = [
+  { label: 'To Watch', value: 'TO_WATCH' },
+  { label: 'Watched', value: 'WATCHED' },
+];
+
 export default function Watchlist() {
+  const [status, setStatus] = useState<'TO_WATCH' | 'WATCHED'>('TO_WATCH');
+
   return (
-    <div>
-      <h1>Watchlist</h1>
+    <div className={styles.watchlistContainer}>
+      <SectionHeader
+        title='My Watchlist'
+        filterTabs={
+          <FilterTabs
+            options={WATCHLIST_STATUSES}
+            active={status}
+            onChange={val => setStatus(val as 'TO_WATCH' | 'WATCHED')}
+          />
+        }
+      />
+      <WatchlistList status={status} />
     </div>
   );
 }
