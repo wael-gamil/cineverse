f965e34fd7035269a4dd4ec975df9ba473760b5c
Wael <131612777+ggwael8@users.noreply.github.com>
2025-08-23 14:27:43 +0300

    refactor: streamline tab change handling in PublicTabsWrapper for improved URL management

--- components/pages/profile/publicTabsWrapper.tsx
+++ components/pages/profile/publicTabsWrapper.tsx
@@ -3,7 +3,7 @@ import PublicReviewsTab from '@/components/pages/profile/publicReviewsTab/public
 import ProfileTabs from './profileTabs';
 import styles from '../../../app/(App)/profile/profile.module.css';
 import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import PublicWatchlistList from '@/components/shared/watchlist/publicWatchlistList';
 
 type PublicTabsWrapperProps = {
@@ -13,16 +13,33 @@ type PublicTabsWrapperProps = {
 export default function PublicTabsWrapper({
   username,
 }: PublicTabsWrapperProps) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const [activeTab, setActiveTab] = useState<
     'reviews' | 'to-watch' | 'watched'
   >('reviews');
 
-  const searchParams = useSearchParams();
   const page = Number(searchParams.get('page') || 1) - 1;
+  const handleTabChange = (newTab: 'reviews' | 'to-watch' | 'watched') => {
+    // Create new URLSearchParams without the page parameter
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.delete('page');
+
+    // Build the new URL
+    const newUrl = newSearchParams.toString()
+      ? `${window.location.pathname}?${newSearchParams.toString()}`
+      : window.location.pathname;
+
+    // Update the URL without the page parameter
+    router.replace(newUrl);
+
+    // Set the active tab
+    setActiveTab(newTab);
+  };
 
   return (
     <div className={styles.tabsWrapper}>
-      <ProfileTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <ProfileTabs activeTab={activeTab} setActiveTab={handleTabChange} />
       {activeTab === 'reviews' && <PublicReviewsTab username={username} />}
       {activeTab === 'to-watch' && (
         <PublicWatchlistList
